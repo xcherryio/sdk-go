b8966e00ceadaadcf31f7e6f8f050d00263d06e5
Quanzheng Long <qlong@indeed.com>
2023-09-25 15:03:24 -0700

    fix file name and comments

--- xdb/process.go
+++ xdb/process.go
@@ -9,13 +9,8 @@ import (
 // Process is a top level concept in XDB
 type Process interface {
 	// GetAsyncStateSchema defines the AsyncStates of the process.
-	// A state can execute some commands (signal/timer) and wait for result
-	// See more details in the AsyncState interface.
-	// It can return an empty list, meaning no states.
-	// There can be at most one startingState.
-	// If there is no startingState or with the default empty state list, the process
-	// will not start any state execution after process stated. Application can still
-	// use RPC to invoke new state execution in the future.
+	// If there is no startingState, the process will not start any state execution after process stated. 
+	// Application can still use RPC to invoke new state execution later.
 	GetAsyncStateSchema() StateSchema
 
 	// GetProcessType defines the processType of this process definition.
