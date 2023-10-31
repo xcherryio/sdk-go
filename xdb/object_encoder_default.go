b8350f9505e0abba1770377b4c548dc1c4c0f4ad
Quanzheng Long <qlong@indeed.com>
2023-10-30 20:51:50 -0700

    remove unused code

--- xdb/object_encoder_default.go
+++ xdb/object_encoder_default.go
@@ -2,10 +2,7 @@ package xdb
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/xdblab/xdb-apis/goapi/xdbapi"
-	"reflect"
-	"strings"
 )
 
 func GetDefaultObjectEncoder() ObjectEncoder {
@@ -13,37 +10,6 @@ func GetDefaultObjectEncoder() ObjectEncoder {
 }
 
 type builtinJsonEncoder struct {
-}
-
-func (b *builtinJsonEncoder) FromGlobalAttributeToDbValue(
-	val interface{}, hint *string,
-) (string, error) {
-	// TODO convert for binary, datetime, etc using hint
-
-	data, err := json.Marshal(val)
-	if err != nil {
-		return "", err
-	}
-	dbVal := strings.Trim(string(data), "\"")
-	return dbVal, nil
-}
-
-func (b *builtinJsonEncoder) FromDbValueToGlobalAttribute(
-	dbQueryValue string, hint *string, resultPtr interface{},
-) error {
-	if dbQueryValue == "" {
-		return nil
-	}
-	rv := reflect.TypeOf(resultPtr)
-	if rv.Kind() != reflect.Ptr {
-		return fmt.Errorf("resultPtr must be a pointer")
-	}
-	ele := rv.Elem()
-	if ele.Kind() == reflect.String {
-		// append "" to make it a valid string json
-		dbQueryValue = "\"" + dbQueryValue + "\""
-	}
-	return json.Unmarshal([]byte(dbQueryValue), resultPtr)
 }
 
 const encodingType = "golangJson"
